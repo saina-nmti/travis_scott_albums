ea496fdab7f8eb1d4a527390ce0017e28029cb3e
saina_nmti <synanemati00@gmail.com>
2025-08-24 17:11:27 +0330

    main

---
+++ cactusjack.py
@@ -0,0 +1,216 @@
+import time
+# saina nmti :D
+
+
+# entry ={
+#     'rager' : '7731'
+# }
+# person = input('Who are you??: ')
+
+# while person not in entry :
+#     print("We've never met you.T_T")
+#     person = input("Try again: ")
+
+
+# entered_pass = input("show us you are a rager: ")
+# while entered_pass != entry[person]:
+#         print('wrong,try again')
+#         entered_pass = input("Try again: ")
+
+# print('                           ')
+print("IT'S LIT!!!!")
+
+print('            ')
+albums = [
+'owl pharaoh' ,
+ 'days before rodeo' ,
+ 'rodeo' ,
+ 'birds in the trap sing mcknight' ,
+ 'huncho jack' ,
+ 'astroword' ,
+ 'jackboys' ,
+ 'utopia' ,
+ 'jackboys2' 
+]
+
+time.sleep(2)
+
+
+
+album_track = {
+    'owl pharaoh': [
+    '1.Meadow Creek',
+	'2.Bad Mood / Shit On You',
+	'3.Upper Echelon feat. T.I. & 2 Chainz',
+	'4.Chaz Interlude feat. Toro y Moi',
+	'5.Uptown feat. A$AP Ferg',
+	'7.Blocka La Flame',
+	'8.Naked feat. Bon Iver',
+	'9.Dance on the Moon feat. Theophilus London & Paul Wall',
+	'10.M.I.A.',
+	'11.Drive feat. James Fauntleroy & Anna Wise',
+	'12.Quintana feat. Wale',
+	'13.Bandz feat. Meek Mill',
+	'14.16 Chapels',
+      ],
+    'days before rodeo' : [
+    "Mamacita feat. Rich Homie Quan & Young Thug",
+    "Quintana Pt. 2",
+    "Drugs You Should Try It",
+    "Don't Play feat. Big Sean & The 1975",
+    "Skyfall feat. Young Thug",
+    "Zombies",
+    "Sloppy Toppy feat. Migos & Peewee Longway",
+    "Basement Freestyle",
+    "Backyard",
+    "Grey",
+    "BACC",
+    "Mo City Flexologist",
+    "Too Many Chances",
+    "Yeah Yeah feat. Young Thug",
+    "Serenade",
+    "Whole Lots Changed",
+    "Hold On feat. Young Thug & Quavo",
+    "Respected",
+    "Naughty",
+    "Too Many Options feat. Young Thug",
+    "Houdini feat. Playboi Carti",
+    "Quintana Pt. 2 (Mike Dean Remix)"
+],
+    'rodeo' : [
+    "Pornography",
+    "Oh My Dis Side feat. Quavo",
+    "3500 feat. Future & 2 Chainz",
+    "Wasted feat. Juicy J",
+    "90210 feat. Kacy Hill",
+    "Pray 4 Love feat. The Weeknd",
+    "Nightcrawler feat. Swae Lee & Chief Keef",
+    "Piss On Your Grave feat. Kanye West",
+    "Flying High feat. Toro y Moi",
+    "Oh My Dis Side (Reprise)",
+    "Apple Pie feat. Justin Bieber",
+    "Impossible",
+    "Maria I'm Drunk feat. Justin Bieber & Young Thug",
+    "Nightcrawler (Reprise)",
+    "Flying High (Reprise)",
+    "Ok Alright feat. ScHoolboy Q",
+    "Never Catch Me feat. Tinashe"
+],
+    'birds in the trap sing mcknight' : [
+    "way back feat. Kid Cudi",
+    "coordinate feat. Blac Youngsta",
+    "through the late night feat. Kid Cudi",
+    "beibs in the trap feat. NAV",
+    "sdp interlude feat. Cassie",
+    "sweet sweet",
+    "outside feat. 21 Savage",
+    "goosebumps feat. Kendrick Lamar",
+    "first take feat. Bryson Tiller",
+    "pick up the phone feat. Young Thug & Quavo",
+    "lose",
+    "guidance feat. K. Forest",
+    "wonderful feat. The Weeknd"
+],
+    'huncho jack' : [
+    "Huncho Jack",
+    "Saint",
+    "Black & Chinese",
+    "Moonlight",
+    "Go",
+    "Dubai Shit",
+    "Modern Slavery",
+    "Where U From",
+    "How U Feel",
+    "Workin",
+    "Extravagant",
+    "Oh My Dis Side",
+    "Saint Laurent",
+    "Motorcycle Patches",
+    "Huncho Jack Outro"
+ ],
+    'astroword' : [
+    "Sicko Mode feat. Drake",
+    "Carousel",
+    "Sicko Mode Outro",
+    "Stop Trying to Be God feat. James Blake, Kid Cudi & Stevie Wonder",
+    "No Bystanders feat. Juice WRLD & Sheck Wes",
+    "Skeletons feat. Tame Impala, Pharrell Williams & The Weeknd",
+    "Wake Up",
+    "5% Tint",
+    "NC-17",
+    "Astrothunder",
+    "Houstonfornication",
+    "Cooped Up feat. Don Toliver",
+    "Can't Say feat. Don Toliver & Shek Wes",
+    "Who? What! feat. Quavo & Takeoff",
+    "Butterfly Effect",
+    "HOUSTONFORNICATION REPRISE",
+    "Coffee Bean"
+ ],
+    'jackboys' : [
+    "JackBoys",
+    "Gang Gang feat. Sheck Wes & Young Thug",
+    "Had Enough feat. Quavo, Offset & Takeoff",
+    "Out West feat. Young Thug",
+    "JACKBOYS INTERLUDE",
+    "Gatti feat. Pop Smoke & Travis Scott",
+    "HIGHEST IN THE ROOM REMIX feat. Rosalia"
+ ],
+    'utopia' : [
+    "Hyaena",
+    "Thank God feat. KayCyy",
+    "Modern Jam feat. Teezo Touchdown",
+    "My Eyes feat. Bon Iver & Sampha",
+    "God's Country",
+    "Sirens",
+    "Meltdown feat. Drake",
+    "Fe!n feat. Playboi Carti & Sheck Wes",
+    "Delresto (Echoes) feat. Beyoncé & Bon Iver",
+    "I Know ?",
+    "Topia Twins feat. 21 Savage & Rob49",
+    "Circus Maximus feat. The Weeknd & Swae Lee",
+    "Parasail feat. Yung Lean & Dave Chappelle",
+    "Skitzo feat. Young Thug",
+    "Lost Forever feat. James Blake & Westside Gunn",
+    "Looove feat. Kid Cudi",
+    "K-Pop feat. The Weeknd & Bad Bunny",
+    "Telekinesis feat. SZA & Future",
+    "Til Further Notice feat. 21 Savage & James Blake"
+ ],
+    'jackboys2' : [
+    "JB2 Radio",
+    "Champain & Vacay feat. Travis Scott & Don Toliver",
+    "2000 Excursion feat. Travis Scott, Sheck Wes & Don Toliver",
+    "Kick Out feat. Travis Scott",
+    "Dumbo feat. Travis Scott",
+    "MM3 feat. SoFaygo",
+    "Velour feat. Don Toliver & Sheck Wes",
+    "Contest feat. Travis Scott & SoFaygo",
+    "ILMB feat. Sheck Wes & Travis Scott",
+    "Where Was You feat. Travis Scott, Playboi Carti & Future",
+    "No Comments feat. Don Toliver",
+    "Beep Beep feat. Travis Scott & SahBabii",
+    "PBT feat. Travis Scott, Tyla & Vybz Kartel",
+    "Shyne feat. Travis Scott & GloRilla",
+    "Outside feat. Travis Scott & YoungBoy Never Broke Again",
+    "Cant Stop feat. Don Toliver, Future & Wallie the Sensei",
+    "Florida Flow feat. Travis Scott & Kodak Black",
+    "Da Wizard Bonus Track feat. Travis Scott"
+ ]
+}
+
+choose = f'well,Choose an album to see  tracks:'
+print(choose)
+print('            ')
+print(albums)
+print('                                 ')
+
+while True:
+     album_name = input('Enter the album name:  ')
+     if album_name in album_track:
+      print(f"Tracks of {album_name} :")
+      for Track in album_track[album_name]:
+            print(Track)
+         
+     else:
+         print("Album not found.Try again: ")
